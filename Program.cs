513600ef45182fef9187b656a48d5575a650fad3
DanDoesInternet <Dan@Dandoesinternet.com>
2017-02-26 18:14:10 -0600

    Modified contestant counts.
    Added individual do loops forcing a check against the user's inputs for
    lastYearNum and thisYearNum and forces them to re-enter if the number is
    outside of the 0-30 inclusive range.
    
    To do:
    1.  Program needs to alert the user when he or she has  entered an
    invalid value.
    2.  Additional instruction so that the user knows their entry must be
    between 0 and 30.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿//Program written by Dan Plummer and Derek West
+//1/.20.17
+//CIS 2620 | Chapter 2 | Case Study #1
+
+//2/15/17
+//CIS 2620 | Chapter 4 | Case Study #1
+
+//2/26/17
+//CIS 2620 | Chapter 5 | Case Study #1
+
+using System;
+using static System.Console;
+
+class GreenvilleRevenue
+{
+    static void Main()
+    {
+        int lastYearNum, thisYearNum;
+        string lastYear, thisYear;
+        double admission = 25.00, revenue;
+
+        do
+        {
+            Write("Enter the number of contestants in last year's competition: ");
+            lastYear = ReadLine();
+            lastYearNum = Int32.Parse(lastYear);
+        }
+        while (lastYearNum < 0 || lastYearNum > 30);
+
+        do
+        {
+            Write("Enter the number of contestants in this year's competition: ");
+            thisYear = ReadLine();
+            thisYearNum = Int32.Parse(thisYear);
+            bool moreThisYear = thisYearNum > lastYearNum;
+            revenue = thisYearNum * admission;
+        }
+        while (thisYearNum < 0 || thisYearNum > 30);
+
+
+        WriteLine();
+        if (thisYearNum > (lastYearNum * 2))
+            WriteLine("The Competition is more than twice as big this year!");
+        else
+            if (thisYearNum > lastYearNum)
+            WriteLine("The competition is bigger than ever!");
+        else
+            if (thisYearNum < lastYearNum)
+            WriteLine("A tighter race this year! Come out and cast your vote.");
+        // WriteLine("There were {0} contestants last year, and {1} contestants this year.", lastYearNum, thisYearNum);
+        // WriteLine("To say that there were more contestants this year than last would be {0}", moreThisYear);
+        WriteLine();
+        WriteLine("Expected revenue for this year's competition is: ${0}", revenue.ToString("F2"));
+    }
+}
